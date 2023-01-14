028fa19bfa786f1f9568f8a0bedbf8aa05e32d6c
Kris Szafranski <kris@thoughtshelter.com>
2023-01-14 16:22:56 -0600

    added input and virtual camera with cinemachine

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour {
 
     [SerializeField]
+    float moveSpeed = 7.5f;
+    [SerializeField]
     bool isAlive = true;
     [SerializeField]
     string myName = "Benjamin";
@@ -16,8 +18,8 @@ public class Mover : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0f, zValue);
     }
 }
