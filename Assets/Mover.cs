6503fb70d553527a2c7c9f6b0d406abf93f00a0d
Kris Szafranski <kris@thoughtshelter.com>
2023-01-14 16:07:56 -0600

    initial commit, video 13

---
+++ Assets/Mover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    [SerializeField]
+    bool isAlive = true;
+    [SerializeField]
+    string myName = "Benjamin";
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, 0f, zValue);
+    }
+}
